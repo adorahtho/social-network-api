cfff91626b0134b2b03c881d3c7ae019dbdd0fa3
thoak90codes <adorahtho@gmail.com>
2023-07-12 23:42:32 -0500

    completed User model

--- models/User.js
+++ models/User.js
@@ -4,9 +4,27 @@ const userSchema = new Schema(
   {
     username: {
       type: String,
-      
-    }
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^\S+@\S+\.\S+$/
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Thought' 
+    }],
+    friends: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User' 
+    }]
   }
 )
 
 const User = model('user', userSchema)
+
+module.exports = User;
