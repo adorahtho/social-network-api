a8eaf541eabdfc7c5178dbb179f01ccc3a51fca4
thoak90codes <adorahtho@gmail.com>
2023-07-16 22:10:31 -0500

    commented out user and thought routes to test GET all users and POST new user. got it to work in mongoDB and insomnia

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,25 +1,25 @@
 const router = require('express').Router();
 const {
   getUsers,
-  getSingleUser,
+  // getSingleUser,
   postNewUser,
-  putUpdateUser,
-  deleteUser,
-  postNewFriend,
-  deleteFriend,
+  // putUpdateUser,
+  // deleteUser,
+  // postNewFriend,
+  // deleteFriend,
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(postNewUser);
 
-router
-  .route('/:userId')
-  .get(getSingleUser)
-  .put(putUpdateUser)
-  .delete(deleteUser);
+// router
+//   .route('/:userId')
+//   .get(getSingleUser)
+//   .put(putUpdateUser)
+//   .delete(deleteUser);
 
-router
-  .route('/:userId/friends/:friendId')
-  .post(postNewFriend)
-  .delete(deleteFriend);
+// router
+//   .route('/:userId/friends/:friendId')
+//   .post(postNewFriend)
+//   .delete(deleteFriend);
 
 module.exports = router;
