507c6f9c655774928c6b3a2db0f27f9f71b1c283
thoak90codes <adorahtho@gmail.com>
2023-07-17 23:51:13 -0500

    created empty array to push user data into it.

--- utils/seed.js
+++ utils/seed.js
@@ -11,6 +11,26 @@ connection.once('open', async () => {
   if (userCheck.length) {
     await connection.dropCollection('users')
   }
-}
 
-// const thought = `This is random thought number ${Math.floor(Math.random() * 200) + 1}.`
+  let thoughtCheck = await connection.db.listCollections({ name: 'thoughts '}).toArray()
+  if (thoughtCheck) {
+    await connection.dropCollection('thoughts')
+  }
+
+  const users = [];
+
+  for (let i = 0; i < 20; i++) {
+    const username = getRandomUsername();
+    const email = `${username}@email.com`;
+    const thoughts = `This is random thought number ${Math.floor(Math.random() * 200) + 1}.`
+    const friends = getRandomUsername();
+
+    users.push({
+      username,
+      email,
+      thoughts,
+      friends,
+    })
+  }
+})
+
