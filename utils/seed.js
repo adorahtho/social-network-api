7bafe3247c07b1c8458a9b8340a4cba135955ac1
thoak90codes <adorahtho@gmail.com>
2023-07-17 23:23:23 -0500

    added delete collections if exist to seed.js file in utils folder

--- utils/seed.js
+++ utils/seed.js
@@ -1,2 +1,16 @@
 const connection = require('../config/connnection')
 const { User, Thought } = require('../models')
+const { getRandomUsername } = require('./data')
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+  // Delete the collections if they exist
+  let userCheck = await connection.db.listCollections({ name: 'users'}).toArray()
+  if (userCheck.length) {
+    await connection.dropCollection('users')
+  }
+}
+
+// const thought = `This is random thought number ${Math.floor(Math.random() * 200) + 1}.`
